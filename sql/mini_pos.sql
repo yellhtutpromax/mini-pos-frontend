38d16c58bd4de8cff51eb976a7770fa1f67c975d
yellhtut <yellhtutpromax@gmail.com>
2025-02-25 17:06:27 +0400

    push sql latest file after created the receipt table

--- sql/mini_pos.sql
+++ sql/mini_pos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 24, 2025 at 02:15 PM
+-- Generation Time: Feb 25, 2025 at 02:05 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `receipts`
+--
+
+CREATE TABLE `receipts` (
+  `id` int(11) NOT NULL,
+  `receipt_code` varchar(255) NOT NULL,
+  `sale_ids` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `payment_id` int(11) NOT NULL,
+  `deposit` decimal(10,2) DEFAULT NULL,
+  `discount` decimal(10,2) DEFAULT NULL,
+  `total_amount` decimal(10,2) NOT NULL,
+  `final_amount` decimal(10,2) NOT NULL,
+  `notes` text NOT NULL,
+  `sell_date` date DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `sales`
 --
 
@@ -33,32 +55,13 @@ CREATE TABLE `sales` (
   `stock_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `unit_price` decimal(10,2) NOT NULL,
-  `total_amount` decimal(10,2) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `sell_date` date NOT NULL,
+  `total_amount` decimal(10,2) NOT NULL,
+  `sell_date` date DEFAULT NULL,
   `notes` text DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `sales`
---
-
-INSERT INTO `sales` (`id`, `voucher_codes`, `stock_id`, `user_id`, `unit_price`, `total_amount`, `quantity`, `sell_date`, `notes`, `created_at`, `updated_at`) VALUES
-(1, '210708189200676', 1, 1, 4000.00, 28000.00, 7, '2025-01-29', NULL, '2025-01-29 08:23:39', '2025-01-29 08:23:39'),
-(2, '210166224142727', 2, 1, 10000.00, 150000.00, 15, '2025-01-29', NULL, '2025-01-29 08:23:47', '2025-01-29 08:23:47'),
-(3, '210216569772195', 4, 1, 150000.00, 2250000.00, 15, '2025-01-29', NULL, '2025-01-29 08:23:53', '2025-01-29 08:23:53'),
-(4, '210005395761675', 5, 1, 15000000.00, 99999999.99, 10, '2025-01-29', NULL, '2025-01-29 08:24:05', '2025-01-29 08:24:05'),
-(5, '210372580678781', 7, 1, 10000.00, 40000.00, 4, '2025-01-29', NULL, '2025-01-29 08:29:07', '2025-01-29 08:29:07'),
-(6, '210046188667913', 7, 1, 10000.00, 50000.00, 5, '2025-01-29', NULL, '2025-01-29 08:29:43', '2025-01-29 08:29:43'),
-(7, '210425744032061', 8, 1, 30000.00, 150000.00, 5, '2025-01-29', 'Here’s a description you can use for the date field, providing some context about what it represents:', '2025-01-29 08:36:00', '2025-01-29 13:11:39'),
-(8, '210996108652633', 8, 1, 30000.00, 150000.00, 5, '2025-01-29', '', '2025-01-29 10:32:54', '2025-01-29 10:32:54'),
-(9, '210665164939386', 8, 1, 30000.00, 2250000.00, 75, '2025-02-17', '', '2025-02-17 05:58:56', '2025-02-17 05:58:56'),
-(10, '210791318907318', 9, 1, 3500.00, 3500.00, 1, '2025-02-20', '', '2025-02-20 10:26:23', '2025-02-20 10:26:23'),
-(11, '210607131935877', 9, 1, 3500.00, 3500.00, 1, '2025-02-20', '', '2025-02-20 10:26:24', '2025-02-20 10:26:24'),
-(12, '210075648267415', 9, 1, 3500.00, 3500.00, 1, '2025-02-20', '', '2025-02-20 10:26:25', '2025-02-20 10:26:25'),
-(13, '210503261725341', 11, 1, 6000.00, 18000.00, 3, '2025-02-20', '', '2025-02-20 10:48:10', '2025-02-20 10:48:10');
 
 -- --------------------------------------------------------
 
@@ -95,7 +98,7 @@ INSERT INTO `stocks` (`id`, `user_id`, `updated_id`, `name`, `barcode`, `buy_amo
 (7, 1, NULL, 'FatherDay\'s Box', '210163288689868', 8000, 10000, 40, '1739856564703.png', 0, '2025-01-28 06:56:49', '2025-02-18 05:29:24'),
 (8, 1, NULL, 'စက္ကူပုံး', '210516866131337', 25000, 30000, 50, '1739856122853.png', 0, '2025-01-28 10:59:32', '2025-02-18 05:22:02'),
 (9, 1, 1, 'Birthday\'s Box', '210299472823490', 3000, 3500, 67, '1739864568637.png', 0, '2025-02-17 13:09:16', '2025-02-18 07:56:46'),
-(11, 1, 1, 'Pizza Box', '210905674772695', 5000, 6000, 27, '1739865165963.png', 0, '2025-02-18 07:29:45', '2025-02-18 07:54:54');
+(11, 1, 1, 'Pizza Box', '210905674772695', 5000, 6000, 30, '1739865165963.png', 0, '2025-02-18 07:29:45', '2025-02-25 08:41:36');
 
 -- --------------------------------------------------------
 
@@ -125,6 +128,12 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `refresh_token`) VALUES
 --
 
 --
+-- Indexes for table `receipts`
+--
+ALTER TABLE `receipts`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `sales`
 --
 ALTER TABLE `sales`
@@ -150,10 +159,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `receipts`
+--
+ALTER TABLE `receipts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `sales`
 --
 ALTER TABLE `sales`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `stocks`
